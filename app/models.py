67a551a1ac47b5cbfad855304cf452bb6f8f6ce8
walters-skn <mickywalters72@gmail.com>
2022-03-02 10:40:43 -0500

    step 6

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash
 
 
 class UserProfile(db.Model):
@@ -12,6 +13,7 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(255))
 
     def is_authenticated(self):
         return True
